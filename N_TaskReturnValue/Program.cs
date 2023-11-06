94037e99bd447e591f94df1f61e862f172031440
Kinara <91768638+kinarajv@users.noreply.github.com>
2023-11-06 14:07:21 +0700

    Add case for Exception Handling differences

--- N_TaskReturnValue/Program.cs
+++ N_TaskReturnValue/Program.cs
@@ -7,13 +7,14 @@ class Program
 		int x = 3;
 		int y = 4;
 		Task<int> task = Task.Run(()=>Add(x,y));
-
+		task.Wait();
 		int result = task.Result;
 		Console.WriteLine($"Result: {result}");
 	}
 	
 	static int Add(int a, int b)
 	{
+		Task.Delay(200);
 		return a + b;
 	}
 }
