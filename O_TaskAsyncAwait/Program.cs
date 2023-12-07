58af5e279b46e72cba5f0a6fd9ead87de622add4
Kinara <91768638+kinarajv@users.noreply.github.com>
2023-12-07 23:33:48 +0000

    Add comparison between Thread Task and AsyncAwait

--- O_TaskAsyncAwait/Program.cs
+++ O_TaskAsyncAwait/Program.cs
@@ -3,17 +3,18 @@ using System.Threading.Tasks;
 
 class Program
 {
-    static async Task Main()
-    {
-        Task result = await Task.Run(()=>AddAsync(3, 4));
-        Console.WriteLine($"Result: {result}");
-    }
+	static async Task Main()
+	{
+		Task result = Task.Run(() => AddAsync(3, 4));
+		await result;
+		Console.WriteLine($"Result: {result}");
+	}
 
-    static async Task AddAsync(int a, int b)
-    {
-        // Simulate an asynchronous operation
-        await Task.Delay(1000);
+	static async Task AddAsync(int a, int b)
+	{
+		// Simulate an asynchronous operation
+		await Task.Delay(1000);
 
-        //return a + b;
-    }
+		//return a + b;
+	}
 }
